74312d6ee9c1f65c25e763205b8e3829c1868ff2
luyer1325 <luyer999999999@gmail.com>
2024-07-01 17:09:53 +0900

    Initial commit

---
+++ src/main/java/com/leeds/manito/manito_pj/ManitoPjApplication.java
@@ -0,0 +1,13 @@
+package com.leeds.manito.manito_pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManitoPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManitoPjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leeds/manito/manito_pj/ManitoPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeds.manito.manito_pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManitoPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
