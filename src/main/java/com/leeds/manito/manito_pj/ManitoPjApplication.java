dd92030b73274bc023f3766dfe044d176431fc04
luyer1325 <luyer999999999@gmail.com>
2024-07-01 21:23:51 +0900

    SETTING
    DB connecting

--- src/main/java/com/leeds/manito/manito_pj/ManitoPjApplication.java
+++ src/main/java/com/leeds/manito/manito_pj/ManitoPjApplication.java
@@ -2,8 +2,11 @@ package com.leeds.manito.manito_pj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// DB 접속 정보 application.properties 파일에 있으며 해당파일 주석 참고
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) // DB없을 시 주석풀고 위 '@SpringBootApplication' 주석처리
+@SpringBootApplication 
 public class ManitoPjApplication {
 
 	public static void main(String[] args) {
