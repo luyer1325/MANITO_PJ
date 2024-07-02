cdd85c136565105068f02fa7d8733552d95678e1
luyer1325 <luyer999999999@gmail.com>
2024-07-02 13:41:01 +0900

    SPRING BOOT ON
    SPRING BOOT 오류 해결 및 DB 접속(이후 테스트 필요)

--- src/main/java/com/leeds/manito/manito_pj/ManitoPjApplication.java
+++ src/main/java/com/leeds/manito/manito_pj/ManitoPjApplication.java
@@ -2,15 +2,24 @@ package com.leeds.manito.manito_pj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 // DB 접속 정보 application.properties 파일에 있으며 해당파일 주석 참고
 //@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) // DB없을 시 주석풀고 위 '@SpringBootApplication' 주석처리
-@SpringBootApplication 
+@SpringBootApplication
 public class ManitoPjApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManitoPjApplication.class, args);
 	}
-
+	//HomeController
+	@RestController
+	public class HomeController{
+		@GetMapping("/")
+		public String index() {
+			return "Hello Wolrd";
+		}
+	}
 }
