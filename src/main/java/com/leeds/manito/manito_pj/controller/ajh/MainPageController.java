208e4de9842e5601f5673b873223d05a0a7541a0
itjuh <jh.2144.9679@gmail.com>
2024-07-10 03:53:26 +0900

    초대하기 페이지 postMapping

--- src/main/java/com/leeds/manito/manito_pj/controller/ajh/MainPageController.java
+++ src/main/java/com/leeds/manito/manito_pj/controller/ajh/MainPageController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.leeds.manito.manito_pj.dto.ManitoInfoDTO;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainPageController {
@@ -18,5 +19,29 @@ public class MainPageController {
     public String make(Model model, ManitoInfoDTO roomInfoDTO) {
         System.out.println("게임생성");
         return "thymeleaf/ajh/makeGame";
+    }
+
+    @PostMapping("/ajh3.do")
+    public String accept(Model model, ManitoInfoDTO roomInfoDTO) {
+        System.out.println("초대받음");
+        return "thymeleaf/ajh/accept";
+    }
+
+    @GetMapping("/ajh4.do")
+    public String start(Model model, ManitoInfoDTO roomInfoDTO) {
+        System.out.println("게임시작");
+        return "thymeleaf/ajh/start";
+    }
+
+    @GetMapping("/ajh5.do")
+    public String end(Model model, ManitoInfoDTO roomInfoDTO) {
+        System.out.println("게임종료");
+        return "thymeleaf/ajh/end";
+    }
+
+    @GetMapping("/ajh6.do")
+    public String end2(Model model, ManitoInfoDTO roomInfoDTO) {
+        System.out.println("게임종료2");
+        return "thymeleaf/ajh/end2";
     }
 }
