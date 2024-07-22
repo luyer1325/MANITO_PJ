8c320ff44949402d7cf1fee9f0b78d673d27bd4e
luyer1325 <luyer999999999@gmail.com>
2024-07-22 23:45:35 +0900

    카카오 로그인 및 로그아웃 구현
    로그아웃 구현 단계

--- src/main/java/com/leeds/manito/manito_pj/controller/ljh/KakaoController.java
+++ src/main/java/com/leeds/manito/manito_pj/controller/ljh/KakaoController.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.leeds.manito.manito_pj.dto.ManitoInfoDTO;
 import com.leeds.manito.manito_pj.service.KakaoService;
+
+import java.util.HashMap;
 
 
 @Controller
@@ -22,9 +23,17 @@ public class KakaoController {
         String accessToken = code;
         System.out.println("어세스코드는 : "+accessToken);
         kakaoservice.test(model);
-        kakaoservice.getAccessToken(code);
+        accessToken = kakaoservice.getAccessToken(code);
+        kakaoservice.getUserInfo(model,accessToken);
+        model.addAttribute("at",accessToken);
+        System.out.println("테스트nick : "+model.getAttribute("nickname"));
+        System.out.println("테스트email : "+model.getAttribute("email"));
         System.out.println("현재 여기");
         return "thymeleaf/ljh/invite";
     }
-    
+    @RequestMapping("/kakao/logout.do")
+    public String kakaoLogout(Model model, ManitoInfoDTO manitoInfoDTO){
+        System.out.println("at : "+model.getAttribute("at"));
+        return "";
+    }
 }
--- src/main/java/com/leeds/manito/manito_pj/service/KakaoService.java
+++ src/main/java/com/leeds/manito/manito_pj/service/KakaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.net.HttpURLConnection;
@@ -13,6 +14,8 @@ import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.OutputStreamWriter;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+
 @Service
 public class KakaoService {
 
@@ -36,6 +39,7 @@ public class KakaoService {
             URL url = new URL(reqUrl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=utf-8"); // 카카오에서의 요청 데이터
+            conn.setDoOutput(true);
 
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             StringBuilder sb = new StringBuilder();
@@ -77,6 +81,91 @@ public class KakaoService {
             e.printStackTrace();
         }
 
-        return "";
+        return accessToken;
+    }
+
+    public void getUserInfo(Model model,String accessToken){
+        String reqUrl = "https://kapi.kakao.com/v2/user/me";
+        try{
+            URL url = new URL(reqUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+            conn.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+            conn.setDoOutput(true);
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("[KakaoApi.getUserInfo] responseCode : "+  responseCode);
+
+            BufferedReader br;
+            if (responseCode >= 200 && responseCode <= 300) {
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+
+            String line = "";
+            StringBuilder responseSb = new StringBuilder();
+            while((line = br.readLine()) != null){
+                responseSb.append(line);
+            }
+            String result = responseSb.toString();
+            System.out.println("responseBody ="+ result);
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+            JsonObject kakaoAccount = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+
+            String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+            String email = kakaoAccount.getAsJsonObject().get("email").getAsString();
+
+            model.addAttribute("nickname", nickname);
+            model.addAttribute("email", email);
+            model.addAttribute("at", accessToken);
+            br.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void kakaoLogout(Model model){
+        String accessToken = (String)model.getAttribute("at");
+        String rUrl = "https://kapi.kakao.com/v1/user/logouthttps://kapi.kakao.com/v1/user/logout";
+        try{
+            URL url = new URL(rUrl);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=utf-8"); // 카카오에서의 요청 데이터
+            conn.setRequestProperty("Authorization", "Bearer "+accessToken);
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("[kakaoApi.kakaologout]responseCode = "+responseCode);
+
+            BufferedReader br;
+            if(responseCode>=200 && responseCode<=300){
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            }else{
+                br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            String line = "";
+            StringBuilder responseSB = new StringBuilder();
+
+            while((line = br.readLine()) != null){
+                responseSB.append(line);
+            }
+            String result = responseSB.toString();
+            System.out.println("[kakao.kakaoLougout] responseBody = "+ result);
+
+            JsonParser jp = new JsonParser();
+            JsonElement je =  jp.parse(result);
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
