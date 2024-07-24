a7e27ed094ae27f07de629f1f3acf53b64595eb4
luyer1325 <luyer999999999@gmail.com>
2024-07-24 20:21:47 +0900

    통합 페이지 생성
    통합페이지 생성 및 validation 수정

--- src/main/java/com/leeds/manito/manito_pj/controller/main/MaintoController.java
+++ src/main/java/com/leeds/manito/manito_pj/controller/main/MaintoController.java
@@ -33,12 +33,12 @@ public class MaintoController {
 
     @RequestMapping("/main.do")
     public String main(Model model, ManitoInfoDTO manitoInfoDTO){
+        kakaoService.getSettings(model);
         return "thymeleaf/main";
     }
 
     @RequestMapping("/invite.do")
     public String invite(Model model, ManitoInfoDTO manitoInfoDTO){
-        kakaoService.getSettings(model); 
         return "thymeleaf/makeGame";
     }
     @RequestMapping("/detail.do")
@@ -53,7 +53,7 @@ public class MaintoController {
         kakaoService.getSettings(model); // Key값 받아오는 서비스
         String accessToken = kakaoService.getAccessToken(session,code,model); // 허용토큰 받는 서비스
         kakaoService.getUserInfo(model,accessToken); //유저 정보 받는 서비스
-        model.addAttribute("rUrl","/detail.do");// redirect Url - kakao.html에서 부모창 redirect url로  이동
+        model.addAttribute("rUrl","/invite.do");// redirect Url - kakao.html에서 부모창 redirect url로  이동
         return "thymeleaf/ljh/kakao";
     }
 
